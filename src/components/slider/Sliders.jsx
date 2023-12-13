1b9deb79b77c4366118c388e0dd30d1c2044e831
Gorkem <gorkemuveyk@yahoo.com>
2023-12-13 21:08:52 +0300

    slider edited

--- src/components/slider/Sliders.jsx
+++ src/components/slider/Sliders.jsx
@@ -1,7 +1,21 @@
 import Slider from "react-slick";
 import SliderItem from "./SliderItem";
+import { GrNext } from "react-icons/gr";
+import { GrPrevious } from "react-icons/gr";
+
+import { useRef } from "react";
 
 const Sliders = () => {
+  const ref = useRef(null);
+
+  const next = () => {
+    ref.current.slickNext();
+  };
+
+  const previous = () => {
+    ref.current.slickPrev();
+  };
+
   const settings = {
     dots: true,
     infinite: true,
@@ -12,8 +26,8 @@ const Sliders = () => {
     slidesToScroll: 1,
   };
   return (
-    <div className="w-10/12 mx-auto mt-5">
-      <Slider {...settings}>
+    <div className="w-10/12 mx-auto mt-5 relative">
+      <Slider {...settings} ref={ref}>
         {/* Custom Slider Item */}
         <SliderItem
           img={"images/apple-watch.png"}
@@ -21,11 +35,26 @@ const Sliders = () => {
             "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis animi nemo omnis illum recusandae optio numquam magni in vel sapiente, voluptates maxime, cupiditate molestiae officia necessitatibus consequatur explicabo aspernatur eaque."
           }
           title={"Apple Watch Series 8"}
+          ref={ref}
         />
 
         {/* Default Slider Item */}
         <SliderItem />
       </Slider>
+      <div className="w-full bg-red-400">
+        <button
+          className="absolute top-1/2 left-0 border border-orange-500 p-2 rounded-md opacity-60 hover:opacity-100 transition-opacity"
+          onClick={previous}
+        >
+          <GrPrevious />
+        </button>
+        <button
+          className="absolute top-1/2 right-0 border border-orange-500 p-2 rounded-md opacity-60 hover:opacity-100 transition-opacity"
+          onClick={next}
+        >
+          <GrNext />
+        </button>
+      </div>
     </div>
   );
 };
