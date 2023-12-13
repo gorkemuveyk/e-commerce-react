427388709ccf00ff539a0f2c2d316bb618708eb3
Gorkem <gorkemuveyk@yahoo.com>
2023-12-13 18:05:39 +0300

    footer and not found page done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,10 @@
 import "./Footer.css";
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-orange-500 text-white flex justify-center items-center">
+      <p>Zamazon | 2023 December</p>
+    </footer>
+  );
 };
 
 export default Footer;
