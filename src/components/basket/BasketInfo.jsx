73f798fa59fc4047509de01839eb86e56774e6d6
Gorkem <gorkemuveyk@yahoo.com>
2023-12-17 17:05:55 +0300

    basket detail done

--- src/components/basket/BasketInfo.jsx
+++ src/components/basket/BasketInfo.jsx
@@ -3,6 +3,14 @@ import { destoryBasket } from "../../redux/slices/basketSlice";
 
 const BasketInfo = () => {
     const dispatch = useDispatch();
+    const keys = Object.keys(localStorage);
+    let totalProduct = 0, cargo = 30, total = 0;
+
+    keys.map((key) => {
+        const tempData = JSON.parse(localStorage.getItem(key));
+        totalProduct += tempData.price * tempData.quantity
+    })
+    total = totalProduct - cargo;
     return (
         <div className="flex flex-col p-2 gap-5 rounded">
             <button onClick={() => dispatch(destoryBasket())} className="bg-orange-500 text-white p-2 rounded">Complete Order</button>
@@ -10,19 +18,19 @@ const BasketInfo = () => {
                 <h1 className="text-start text-xl my-4 w-full">Order Summary</h1>
                 <div className="flex flex-row justify-between p-3 w-full items-center border-b">
                     <p>Total Product</p>
-                    <span className="text-orange-400 font-bold text-xl">100</span>
+                    <span className="text-orange-400 font-bold text-xl">${totalProduct}</span>
                 </div>
                 <div className=" border-b flex flex-row justify-between p-3 w-full items-center">
                     <p>Cargo Total</p>
-                    <span className="text-orange-400 font-bold text-xl">100</span>
+                    <span className="text-orange-400 font-bold text-xl">${cargo}</span>
                 </div>
                 <div className=" border-b flex flex-row justify-between p-3 w-full items-center">
-                    <p>Free shipping on this order</p>
-                    <span className="text-orange-400 font-bold text-xl">-100</span>
+                    <p>Free cargo on this order</p>
+                    <span className="text-orange-400 font-bold text-xl">-${cargo}</span>
                 </div>
                 <div className=" border-b flex flex-row justify-between p-3 w-full items-center">
                     <p>Total</p>
-                    <span className="text-orange-400 font-bold text-xl">100</span>
+                    <span className="text-orange-400 font-bold text-xl">${total + cargo}</span>
                 </div>
                 <div className="flex justify-between w-full  items-center">
                     <input className="w-full p-2 rounded m-2 border border-orange-500 outline-none" type="text" placeholder="Enter e coupon code" />
